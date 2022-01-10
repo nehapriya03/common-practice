3161eccf4116e7f862caf847fee8fa1dc1590e97
nehapriya03 <56948455+nehapriya03@users.noreply.github.com>
2022-01-10 00:20:32 -0500

    final commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+// import faker from "faker";
+import ContainerDetail from "./ContainerDetail";
+import ApprovalCard from './ApprovalCard'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+      <ContainerDetail
+        author="Sam"
+        date="Toady at 6 pm"
+        text="This is good!"
+      ></ContainerDetail>
+      </ApprovalCard>
+      
+      <ApprovalCard>
+      <ContainerDetail
+        author="Alex"
+        date="Toady at 7 pm"
+        text="This is good!"
+      ></ContainerDetail>
+      </ApprovalCard>
+    </div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.querySelector("#root"));
